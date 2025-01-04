79bea6dfb999152b1b5291c996357963f42af6cb
IPCSGLOBALHQKOCHI <ipcsdeveloper@gmail.com>
2025-01-04 15:30:18 +0530

    added mail in nodemailer

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.post("/api/submitform", async (req, res) => {
   // Mail options
   const mailOptions = {
     from: "info@ipcsglobal.com",
-    to: "ipcsglobalindia@gmail.com",
+    to: "ipcsglobalindia@gmail.com,dmmanager.ipcs@gmail.com",
     subject: "Lead Form Submission",
     html: emailContent,
   };
